bef143154a587befd907f791ad2302112cf26b4d
Juan Esteban Matheo López Ardila <111018899+juanesteban-matheo@users.noreply.github.com>
2024-10-25 18:47:26 -0500

    Usuario.java completo(?)
    Por favor revisar para posibles mejoras o cmabios

---
+++ Proyecto1-main/Proyecto_1/src/clases/LearningPathTree/Actividad.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package clases.LearningPathTree;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * 
+ */
+public class Actividad {
+	/** ATRIBUTOS */
+	public String nombre;
+	public String tipo;
+	public String descripcion;
+	public String objetivo;
+	public String nivelDificultad;
+	public int duracion;
+	public List<Actividad> actividadesPrevias;
+	public Date fechaLimite;
+	public String resultado;
+	public boolean obligatorio;
+	public List<String> reseñas;
+	public List<Float> rating;
+	public String autor;
+	
+	/** FUNCIONES */
+	public void completarActividad() {
+		
+	}
+}
---
+++ Proyecto1-main/Proyecto_1/src/clases/LearningPathTree/LearningPath.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package clases.LearningPathTree;
+import java.util.HashMap;
+/**
+ * Clase del Learning Path
+ */
+public class LearningPath {
+	/** ATRIBUTOS */
+	public String titulo;
+	public String descripcion;
+	public String objetivos;
+	public String nivelDificultad;
+	public int duracion;
+	public float rating;
+	public String fechaDeCreacion;
+	public String fechaDeModificacion;
+	public String vercion;
+    public HashMap<String, String> estructura;
+	/** FUNCIONES */
+	public void agregarActividad() {
+		
+	}
+	
+	public void editarLearningPath() {
+		
+	}
+	
+}
