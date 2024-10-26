a0d836b1cc2413d53945a2ce874a15477e9233ed
Juan Esteban Matheo López Ardila <111018899+juanesteban-matheo@users.noreply.github.com>
2024-10-25 22:59:46 -0500

    ENTREGA 2 PROYECTO 1
    Documentos finales de la 2da entrega del Proyecto 1 de DPOO

--- Proyecto1-main/Proyecto_1/src/clases/LearningPathTree/Actividad.java
+++ Proyecto1-main/Proyecto_1/src/clases/LearningPathTree/Actividad.java
@@ -4,6 +4,7 @@
 package clases.LearningPathTree;
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * 
@@ -17,7 +18,7 @@ public class Actividad {
 	public String nivelDificultad;
 	public int duracion;
 	public List<Actividad> actividadesPrevias;
-	public Date fechaLimite;
+	public String fechaLimite;
 	public String resultado;
 	public boolean obligatorio;
 	public List<String> reseñas;
@@ -25,7 +26,66 @@ public class Actividad {
 	public String autor;
 	
 	/** FUNCIONES */
-	public void completarActividad() {
+	public void crearActividad() {
 		
+		Scanner input = new Scanner(System.in);
+		
+		// NOMBRE
+        System.out.println("Ingrese el NOMBRE de la actividad: ");
+        String name = input.next();
+        this.nombre = name;
+        
+        // TIPO
+        System.out.println("Ingrese el TIPO de la actividad: ");
+        String type = input.next();
+        this.tipo = type;
+        
+        // DESCRIPCIÓN
+        System.out.println("ingrese la DESCRIPCIÓN de la actividad como un párrafo: ");
+        String description = input.next();
+        this.descripcion = description;
+        
+        // DESCRIPCIÓN
+        System.out.println("ingrese el OBJETIVO de la actividad: ");
+        String objective = input.next();
+        this.objetivo = objective;
+        
+        // NIVEL DIFICULTAD
+        System.out.println("ingrese la DIFICULTAD de la actividad: ");
+        String level = input.next();
+        this.descripcion = level;
+        
+        // DURACIÓN
+        System.out.println("ingrese la DURACIÓN de la actividad en minutos como un número entero: ");
+        int duration = input.nextInt();
+        this.duracion = duration;
+        
+        // FECHA LÍMITE
+        System.out.println("ingrese la FECHA LÍMITE de la actividad: ");
+        String limitDate = input.next();
+        this.fechaLimite = limitDate;
+        
+        // OBLIGATORIO
+        System.out.println("Es esta actividad OBLIGATORIA? [Y/N]: ");
+        String required = input.next();
+        if (required == "Y") 
+        {this.obligatorio = true;} 
+        else
+        {this.obligatorio = false;}
+        
+     // NOMBRE
+        System.out.println("Ingrese su nombre para registrar AUTOR: ");
+        String author = input.next();
+        this.autor = author;
+        
+        input.close();
+	}
+	
+	public void completarActividad() {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Ingrese el resultado de la actividad: ");
+		String result = input.next();
+		this.resultado = result;
+		input.close();
 	}
 }
--- Proyecto1-main/Proyecto_1/src/clases/LearningPathTree/LearningPath.java
+++ Proyecto1-main/Proyecto_1/src/clases/LearningPathTree/LearningPath.java
@@ -3,6 +3,8 @@
  */
 package clases.LearningPathTree;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Scanner;
 /**
  * Clase del Learning Path
  */
@@ -16,15 +18,35 @@ public class LearningPath {
 	public float rating;
 	public String fechaDeCreacion;
 	public String fechaDeModificacion;
-	public String vercion;
+	public String version;
     public HashMap<String, String> estructura;
+    public List<Actividad> listaActividades;
 	/** FUNCIONES */
-	public void agregarActividad() {
-		
-	}
 	
-	public void editarLearningPath() {
+	public void editarLearningPath(Actividad actividad) {
 		
+		boolean processing = true; 
+		while (processing) 
+		{
+			System.out.println("1.) Agregar Actividad a LearningPath");
+			System.out.println("2.) Eliminar Actividad de LearningPath");
+			Scanner input = new Scanner(System.in);
+			System.out.println("Seleccionar opción: ");
+			String answer = input.next();
+			int choice =Integer.parseInt(answer);
+			input.close();
+		
+			if (choice == 1) {
+				listaActividades.add(actividad);
+				System.out.println("La Actividad ha sido añadida correctamente.");
+				processing = false;
+			} else if (choice == 2) {
+				listaActividades.remove(actividad);
+				System.out.println("La Actividad ha sido eliminada correctamente.");
+				processing = false;
+			} else  {
+				System.out.println("Valor ingresado no funciona, intente otra vez.");
+			}
+		}
 	}
-	
 }
